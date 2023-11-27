32441a04ec0fd2813d2c33380fb9750266692626
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-11-27 12:11:02 -0800

    added clicked style

--- assets/main.ts
+++ assets/main.ts
@@ -1,9 +1,11 @@
 const leftIds: string[] = ['lbtn1','lbtn2','lbtn3']
 const rightIds: string[] = ['rbtn1','rbtn2','rbtn3']
 
+let isClicked:boolean = false;
+
 const leftBtns: HTMLElement[] = leftIds.map(id => {
 	let element = document.getElementById(id);
-	if(element) {
+	if (element) {
 		return element as HTMLElement;
 	} else {
 		throw new Error('Not found');
@@ -18,6 +20,23 @@ const rightBtns: HTMLElement[] = rightIds.map(id => {
 		throw new Error('Not found');
 	}
 });
+
+leftBtns.forEach(button => {
+	button.addEventListener('click', () => {
+	if (!isClicked){
+		button.classList.remove('button');
+		button.classList.add('clickedStyles');
+		button.removeAttribute('id');
+	} else {
+		button.classList.remove('clickedStyles');
+		button.classList.add('button');
+		button.setAttribute('id',leftIds[leftBtns.indexOf(button)]);
+	}
+	isClicked = !isClicked;
+
+	});
+});
+
 
 console.log(leftBtns);
 console.log(rightBtns);
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 const leftIds = ['lbtn1', 'lbtn2', 'lbtn3'];
 const rightIds = ['rbtn1', 'rbtn2', 'rbtn3'];
+let isClicked = false;
 const leftBtns = leftIds.map(id => {
     let element = document.getElementById(id);
     if (element) {
@@ -18,6 +19,21 @@ const rightBtns = rightIds.map(id => {
     else {
         throw new Error('Not found');
     }
+});
+leftBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!isClicked) {
+            button.classList.remove('button');
+            button.classList.add('clickedStyles');
+            button.removeAttribute('id');
+        }
+        else {
+            button.classList.remove('clickedStyles');
+            button.classList.add('button');
+            button.setAttribute('id', leftIds[leftBtns.indexOf(button)]);
+        }
+        isClicked = !isClicked;
+    });
 });
 console.log(leftBtns);
 console.log(rightBtns);
