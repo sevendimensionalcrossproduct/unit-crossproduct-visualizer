3e1cd09f41865672c7dc2b9fe0db747fc0313acf
serna <114138913+sevendimensionalcrossproduct@users.noreply.github.com>
2023-11-26 20:13:09 -0800

    Initialized element array

--- assets/main.ts
+++ assets/main.ts
@@ -1,3 +1,23 @@
-let a:number = 1;
+const leftIds: string[] = ['lbtn1','lbtn2','lbtn3']
+const rightIds: string[] = ['rbtn1','rbtn2','rbtn3']
 
-console.log(a + a);
+const leftBtns: HTMLElement[] = leftIds.map(id => {
+	let element = document.getElementById(id);
+	if(element) {
+		return element as HTMLElement;
+	} else {
+		throw new Error('Not found');
+	}
+});
+
+const rightBtns: HTMLElement[] = rightIds.map(id => {
+	let element = document.getElementById(id);
+	if(element) {
+		return element as HTMLElement;
+	} else {
+		throw new Error('Not found');
+	}
+});
+
+console.log(leftBtns);
+console.log(rightBtns);
--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 "use strict";
-let a = 1;
-console.log(a + a);
+const leftIds = ['lbtn1', 'lbtn2', 'lbtn3'];
+const rightIds = ['rbtn1', 'rbtn2', 'rbtn3'];
+const leftBtns = leftIds.map(id => {
+    let element = document.getElementById(id);
+    if (element) {
+        return element;
+    }
+    else {
+        throw new Error('Not found');
+    }
+});
+const rightBtns = rightIds.map(id => {
+    let element = document.getElementById(id);
+    if (element) {
+        return element;
+    }
+    else {
+        throw new Error('Not found');
+    }
+});
+console.log(leftBtns);
+console.log(rightBtns);
